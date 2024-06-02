9e4bfec3b737652bc7e71342c98b08d4bddc9536
sbx_user1051 <sbx_user1051@169.254.58.101>
2024-06-02 09:02:34 +0000

    Sun Jun 02 09:02:34 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,39 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
+        private static final Map<Character, int[]> specialOffer = new HashMap<>();
+        private static final Map<Character, Integer> prices = new HashMap<>();
+
+        static{
+            //prices
+            prices.put('A', 50);
+            prices.put('B', 30);
+            prices.put('C', 20);
+            prices.put('D', 15);
+
+        }
+
+        specialOffer.put('A', new int[]{3,130});
+        specialOffer.put('B', new int[]{2,45});
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        //   throw new SolutionNotImplementedException();
+        if(skus == null || skus.isEmpty()){
+            return 0;
+        }
+        Map<Character, Integer> itemConter = new HashMap<>();
+        for(char sku: skus.toCharArray()){
+            if(!prices.containsKey(sku)){
+                return -1;
+            }
+            itemConter.put(sku,itemConter.getOrDefault(sku,0)+1);
+        }
+        return 0;
     }
+
 }
+
